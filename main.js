4bf1abe693be6086fa47f907a95ed4f3ad894ba4
vellaiyan <vellaiyan211464@gmail.com>
2022-11-27 17:15:14 +0530

    code changes

--- main.js
+++ main.js
@@ -4,10 +4,7 @@ window.addEventListener("load", () => {
   userDetails = JSON.parse(localStorage.getItem("userDetails")) || [];
 
   validateUser();
-  userName = document.querySelector(".userName");
-  userName.innerHTML = userDetails[0].userName;
-  orderedCount = document.getElementById("count");
-  orderedCount.innerHTML = cartItems.length;
+  updateDetails();
   fetchApi().then((response) => {
     localStorage.setItem("items", JSON.stringify(response));
   });
@@ -15,6 +12,15 @@ window.addEventListener("load", () => {
   displayItems();
 });
 
+//To update the details
+function updateDetails() {
+  userName = document.querySelector(".userName");
+  userName.innerHTML = userDetails[0].userName;
+  orderedCount = document.getElementById("count");
+  orderedCount.innerHTML = cartItems.length;
+}
+
+//To register the new user.
 function registerUser() {
   userName = document.querySelector("#user-Name");
   submit = document.querySelector("submit");
@@ -34,6 +40,7 @@ function registerUser() {
   }
 }
 
+//To get the api
 async function fetchApi() {
   const response = await fetch(
     "https://raw.githubusercontent.com/vellaiyan/frontEndTraining/24-11-2022/.mockend.json"
@@ -42,6 +49,7 @@ async function fetchApi() {
   return data;
 }
 
+//To validate the current user.
 function validateUser() {
   sectionContainer = document.querySelector(".top-section");
   container = document.querySelector(".container");
@@ -57,86 +65,70 @@ function validateUser() {
   }
 }
 
+//To log out.
 function logout() {
   localStorage.removeItem("userDetails");
   document.location.reload();
 }
+
+//To display items.
 function displayItems() {
   const topSection = document.querySelector("#top-section");
   topSection.innerHTML = "";
-  /* left side of the top section */
-  const leftSide = document.createElement("div");
-  leftSide.classList.add("left-side");
+
+  const leftSide = createEle("div", "left-side");
 
   topSection.appendChild(leftSide);
-  const rightSide = document.createElement("div");
-  rightSide.classList.add("right-side");
+  const rightSide = createEle("div", "right-side");
   rightSide.classList.add("items");
-
   topSection.appendChild(rightSide);
 
   /* right side of the top section */
   items.forEach((element) => {
-    console.log(element.cost);
-    const itemDivision = document.createElement("div");
-    itemDivision.classList.add("item-division");
+    const itemDivision = createEle("div", "item-division");
+    const firstRow = createEle("div", "first-row");
+    const item = createEle("div", "item");
+    const itemContainer = createEle("div", "item-container");
 
-    const firstRow = document.createElement("div");
-    firstRow.classList.add("first-row");
-
-    const first = document.createElement("div");
-    first.classList.add("first");
-
-    const itemContainer = document.createElement("div");
-    itemContainer.classList.add("item-container");
-
-    const firstItem = document.createElement("img");
+    const firstItem = createEle("img", "first-item");
     firstItem.setAttribute("src", element.url);
-    firstItem.classList.add("first-item");
     itemContainer.appendChild(firstItem);
 
-    const hotButton = document.createElement("button");
-    hotButton.classList.add("hot");
+    const hotButton = createEle("button", "hot");
     hotButton.innerHTML = "HOT";
     itemContainer.appendChild(hotButton);
 
     if (element.sale != "no") {
-      const saleButton = document.createElement("button");
+      const saleButton = createEle("button", "sale");
       saleButton.innerHTML = "SALE";
-      saleButton.classList.add("sale");
       itemContainer.appendChild(saleButton);
     }
 
-    const addButton = document.createElement("div");
-    addButton.classList.add("add-button");
+    const addButton = createEle("button", "add-button");
     addButton.innerHTML = "Add";
     itemContainer.appendChild(addButton);
 
-    const itemTitle = document.createElement("div");
-    itemTitle.classList.add("item-title");
+    const itemTitle = createEle("div", "item-title");
     itemContainer.appendChild(itemTitle);
 
-    const itemTitleText = document.createElement("h2");
-    itemTitleText.classList.add("item-name");
+    const itemTitleText = createEle("h2", "item-name");
     itemTitleText.innerHTML = element.ItemName;
     itemTitle.appendChild(itemTitleText);
 
-    const price = document.createElement("p");
-    price.classList.add("price");
+    const price = createEle("p", "price");
     price.innerHTML = "$" + element.cost;
     itemTitle.appendChild(price);
 
     if (element.offer != "") {
-      const offer = document.createElement("span");
-      offer.classList.add("offer");
+      const offer = createEle("span", "offer");
       offer.innerHTML = "$" + element.offer;
       price.appendChild(offer);
     }
 
-    first.appendChild(itemContainer);
-    first.appendChild(itemTitle);
+    item.appendChild(itemContainer);
+    item.appendChild(itemTitle);
 
-    firstRow.appendChild(first);
+    firstRow.appendChild(item);
 
     itemDivision.appendChild(firstRow);
     rightSide.appendChild(itemDivision);
@@ -145,12 +137,27 @@ function displayItems() {
       const cartItem = {
         itemName: element.ItemName,
         cost: element.cost,
+        quantity: 1,
+        itemCost: element.cost,
       };
-      console.log(cartItem);
-      cartItems.push(cartItem);
-      localStorage.setItem("cartItems", JSON.stringify(cartItems));
-      orderedCount = document.getElementById("count");
-      orderedCount.innerText = cartItems.length;
+      isPresent = 0;
+      cartItems.forEach((item) => {
+        if (item.itemName === cartItem.itemName) {
+          item.quantity += cartItem.quantity;
+          item.cost = item.quantity * element.cost;
+          localStorage.setItem("cartItems", JSON.stringify(cartItems));
+          isPresent = 1;
+        }
+      });
+
+      if (isPresent == 0) {
+        console.log(cartItem);
+        cartItems.push(cartItem);
+        localStorage.setItem("cartItems", JSON.stringify(cartItems));
+        orderedCount = document.getElementById("count");
+        orderedCount.innerText = cartItems.length;
+        isPresent;
+      }
     });
   });
 }
@@ -166,48 +173,47 @@ function openOrders() {
 
   cartItems.forEach((cartItem, index) => {
     totalCost += parseInt(cartItem.cost);
-    const orderedItem = document.createElement("div");
-    orderedItem.classList.add("orderedItem");
+    const orderedItem = createEle("div", "orderedItem");
 
-    const orderedItemIndex = document.createElement("input");
-    orderedItemIndex.classList.add("text");
+    const orderedItemIndex = createEle("input", "text");
     orderedItemIndex.classList.add("orderedItemIndex");
     orderedItemIndex.type = "text";
     orderedItemIndex.value = index + 1;
     //orderedItemIndex.classList.add("orderedItemIndex");
     //have to set the index of the item
 
-    const indexElement = document.createElement("div");
-    indexElement.classList.add("index");
+    const indexElement = createEle("div", "index");
     indexElement.appendChild(orderedItemIndex);
 
     /* index completed           */
 
-    const orderedItemName = document.createElement("input");
-    orderedItemName.classList.add("text");
+    const orderedItemName = createEle("input", "text");
     orderedItemName.classList.add("orderedItemName");
     orderedItemName.type = "text";
     orderedItemName.value = cartItem.itemName;
 
-    const nameElement = document.createElement("div");
-    nameElement.classList.add("cartItemName");
+    const nameElement = createEle("div", "cartItemName");
     nameElement.appendChild(orderedItemName);
 
     /* name completed */
 
-    const deleteElement = document.createElement("button");
-    deleteElement.classList.add("delete");
+    const quantity = createEle("input", "quantity");
+    quantity.type = "number";
+    quantity.value = cartItem.quantity;
+
+    const quantityElement = createEle("div", "quantity");
+    quantityElement.appendChild(quantity);
+
+    const deleteElement = createEle("button", "delete");
     deleteElement.innerHTML =
       "<i style = 'font-size: 30px; color: #ffeb3b; weight: 900' class='fa fa-trash-o'></i>";
 
-    const orderedItemPrice = document.createElement("input");
-    orderedItemPrice.classList.add("text");
+    const orderedItemPrice = createEle("input", "test");
     orderedItemPrice.classList.add("orderedItemPrice");
     orderedItemPrice.type = "text";
     orderedItemPrice.value = "$" + cartItem.cost;
 
-    const priceElement = document.createElement("div");
-    priceElement.classList.add("cartItemPrice");
+    const priceElement = createEle("div", "cartItemPrice");
     priceElement.appendChild(orderedItemPrice);
 
     /* price completed */
@@ -215,8 +221,20 @@ function openOrders() {
     orderedItem.appendChild(indexElement);
     orderedItem.appendChild(nameElement);
     orderedItem.appendChild(deleteElement);
+    orderedItem.appendChild(quantityElement);
     orderedItem.appendChild(priceElement);
     cartItemsLayout.appendChild(orderedItem);
+
+    quantityElement.addEventListener("change", () => {
+      let input = event.target;
+
+      cartItem.cost = input.value * cartItem.itemCost;
+      cartItem.quantity = input.value;
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
+      openOrders();
+    });
+
+    deleteItem(deleteElement, cartItems);
 
     deleteElement.addEventListener("click", () => {
       cartItems = cartItems.filter((t) => t != cartItem);
@@ -232,7 +250,15 @@ function openOrders() {
   totalCostelement.innerHTML = "$" + totalCost.toString();
 }
 
+function deleteItem(deleteElement, cartItems) {}
+
 function closeOrders() {
   ordersLayout = document.getElementById("ordered-items");
   ordersLayout.style.display = "none";
+}
+
+function createEle(ele, classlist) {
+  element = document.createElement(ele);
+  element.classList.add(classlist);
+  return element;
 }
